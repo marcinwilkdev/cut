59e91824583165e0e415801419d3bed778a9bf74
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-05-22 22:19:31 +0200

    create text_message for message passing between Reader and Analyzer

---
+++ text_message.c
@@ -0,0 +1,63 @@
+#include <string.h>
+
+#include "text_message.h"
+
+struct Text_message {
+    bool empty;
+    char text[];
+};
+
+Text_message* text_message_empty(void) {
+    register size_t const message_size = sizeof(Text_message);
+
+    register Text_message* const message = malloc(message_size);
+
+    if (message == NULL)
+        return NULL;
+
+    *message = (Text_message){
+        .empty = true,
+    };
+
+    return message;
+}
+
+bool text_message_is_empty(register Text_message const* const message) {
+    if (message == NULL)
+        return true;
+
+    return message->empty;
+}
+
+char const* text_message_message(register Text_message const* message) {
+    if (text_message_is_empty(message))
+        return NULL;
+
+    return message->text;
+}
+
+Text_message* text_message_new(register size_t const len,
+                               char const message_text[const len]) {
+    if (len == 0)
+        return NULL;
+
+    register size_t const message_size =
+        sizeof(Text_message) + sizeof(char[len]);
+
+    register Text_message* const message = malloc(message_size);
+
+    if (message == NULL)
+        return NULL;
+
+    *message = (Text_message){
+        .empty = false,
+    };
+
+    memcpy(message->text, message_text, sizeof(char[len]));
+
+    return message;
+}
+
+void text_message_delete(register Text_message* const message) {
+    free(message);
+}
---
+++ text_message.h
@@ -0,0 +1,27 @@
+#ifndef TEXT_MESSAGE_H
+#define TEXT_MESSAGE_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/* Used for message passing between Reader and Analyzer threads. Can hold
+ * message or be empty. If message is empty it indicates that program
+ * has to end. */
+typedef struct Text_message Text_message;
+
+/* Creates new Text_message with text in it. Returns NULL if len is equal
+ * to 0 or if couldn't malloc memory for message. */
+Text_message* text_message_new(size_t len, char const text[len]);
+/* Safely deletes message and deallocates memory. */
+void text_message_delete(Text_message* message);
+
+/* Creates special Text_message of type empty which indicates that
+ * the program has to end. */
+Text_message* text_message_empty(void);
+
+/* Checks if Text_message is empty. Returns true if message is NULL. */
+bool text_message_is_empty(Text_message const* message);
+/* Returns text from Text_message or NULL if Text_message is NULL or empty. */
+char const* text_message_message(Text_message const* message);
+
+#endif
