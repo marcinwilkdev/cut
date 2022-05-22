7f918b7a351911f1f5fe31e3a4defd0fc558041e
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-05-22 22:20:55 +0200

    add watcher and text_message tests to main_test so that they are also run during testing

--- main_test.c
+++ main_test.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 
 #include "channel_test.h"
+#include "watcher_test.h"
+#include "text_message_test.h"
 
 int main(void) {
     channel_creation_test();
     channel_push_pop_test();
     channel_push_pop_multithread_test();
+
+    watcher_creation_test();
+    watcher_notifying_test();
+
+    text_message_creation_test();
+    text_message_empty_test();
 
     puts("All tests passed succesfully.");
 
