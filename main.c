ec59299d60e820f45dccc3555011db39688ac255
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-05-23 12:25:50 +0200

    add logger to main.c

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 #include "printer_test.h"
 #include "reader.h"
 #include "text_message.h"
+#include "logger.h"
 
 #define CHANNEL_SIZE 1000
 
@@ -37,6 +38,14 @@ inline static void* printer_thread_start(void* const p) {
     return NULL;
 }
 
+inline static void* logger_thread_start(void* const l) {
+    register Logger* const logger = l;
+
+    logger_start(logger);
+
+    return NULL;
+}
+
 int main(void) {
     Channel* const text_channel =
         channel_new(CHANNEL_SIZE, sizeof(Text_message*));
@@ -61,11 +70,16 @@ int main(void) {
         .log_channel = log_channel,
     };
 
-    pthread_t threads[3];
+    Logger logger = {
+        .log_channel = log_channel,
+    };
+
+    pthread_t threads[4];
 
     pthread_create(&threads[0], NULL, reader_thread_start, &reader);
     pthread_create(&threads[1], NULL, analyzer_thread_start, analyzer);
     pthread_create(&threads[2], NULL, printer_thread_start, &printer);
+    pthread_create(&threads[3], NULL, logger_thread_start, &logger);
 
     nanosleep(&(struct timespec){.tv_sec = 3}, NULL);
 
@@ -74,22 +88,9 @@ int main(void) {
     pthread_join(threads[0], NULL);
     pthread_join(threads[1], NULL);
     pthread_join(threads[2], NULL);
+    pthread_join(threads[3], NULL);
 
     analyzer_delete(analyzer);
-
-    for (;;) {
-        Log_message* log_message = NULL;
-
-        channel_pop(log_channel, &log_message);
-
-        if (log_message_is_empty(log_message)) {
-            log_message_delete(log_message);
-
-            break;
-        }
-
-        log_message_delete(log_message);
-    }
 
     channel_delete(text_channel);
     channel_delete(log_channel);
